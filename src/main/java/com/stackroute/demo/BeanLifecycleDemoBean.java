20af0e3e0cd26c1b25979c5d0248494c9fcd2b35
adityan.a <adityan.a@cgi.com>
2019-09-24 16:32:00 +0530

    Task 5

---
+++ src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
@@ -0,0 +1,26 @@
+package com.stackroute.demo;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBean");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean");
+
+    }
+
+    public void customInit(){
+        System.out.println("Custom Init");
+    }
+
+    public void customDestroy(){
+        System.out.println("Custom Destroy");
+    }
+}
+
